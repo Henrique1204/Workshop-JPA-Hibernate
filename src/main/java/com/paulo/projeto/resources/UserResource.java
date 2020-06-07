4b0b34ade285a538222729b30f9e5d61b8125259
Paulo <pauloh16.pdr@gmail.com>
2020-06-07 03:04:31 -0300

    Registrar componentes

--- src/main/java/com/paulo/projeto/resources/UserResource.java
+++ src/main/java/com/paulo/projeto/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.paulo.projeto.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paulo.projeto.entidades.User;
+import com.paulo.projeto.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource
 {
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll()
+	public ResponseEntity<List<User>> findAll()
 	{
-		User u = new User(1L, "Maria", "maria@gmail.com", "11111111", "senha");
-		return ResponseEntity.ok().body(u);
+		List<User> lista = service.findAll();
+		return ResponseEntity.ok().body(lista);
+	}
+
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id)
+	{
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
