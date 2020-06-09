f19e569fe3ab764a4d5795d6689adc50b58933ae
Paulo <pauloh16.pdr@gmail.com>
2020-06-08 22:25:39 -0300

    Adicionar entidade "Payment" e associacao um-para-um

--- src/main/java/com/paulo/projeto/config/TestConfig.java
+++ src/main/java/com/paulo/projeto/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.paulo.projeto.entidades.Category;
 import com.paulo.projeto.entidades.Order;
 import com.paulo.projeto.entidades.OrderItem;
+import com.paulo.projeto.entidades.Payment;
 import com.paulo.projeto.entidades.Product;
 import com.paulo.projeto.entidades.User;
 import com.paulo.projeto.entidades.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPreco());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 }
