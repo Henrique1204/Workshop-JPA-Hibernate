26849d9dc5d2e40f88cf8e2bb3746cbe187d6945
Paulo <pauloh16.pdr@gmail.com>
2020-06-09 04:05:29 -0300

    Tratar exceptions do metodo "Delete"

--- src/main/java/com/paulo/projeto/services/UserService.java
+++ src/main/java/com/paulo/projeto/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.paulo.projeto.entidades.User;
 import com.paulo.projeto.repositories.UserRepository;
+import com.paulo.projeto.services.exceptions.DatabaseException;
 import com.paulo.projeto.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -34,7 +37,18 @@ public class UserService
 
 	public void delete(Long id)
 	{
-		repository.deleteById(id);
+		try
+		{
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e)
+		{
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e)
+		{
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj)
